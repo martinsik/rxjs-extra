f1014ddec4e7f61b01bd2254e46bda8626248f74
Martin Sikora <martin.sikora.ahoj@gmail.com>
2017-03-11 12:25:46 +0100

    takeWhileInclusive operator wip

--- README.md
+++ README.md
@@ -1,2 +1,18 @@
 # rxjs-plus
 Collection of additional RxJS 5 operators
+
+# Testing
+
+This repository uses the same `mocha` testing helpers as RxJS 5 including rendering marble diagrams.
+
+To run the tests this repo needs to download the RxJS 5 archive, unpack it, copy helper scripts and patch them by [files from `spec-patch`](https://github.com/martinsik/rxjs-plus/tree/master/spec-patch).
+
+In order to run tests do the following tests:
+
+```
+$ clone https://github.com/martinsik/rxjs-plus
+$ cd rxjs-plus
+$ npm i
+$ npm run spec-setup && npm run test-build
+$ npm run test
+```
---
+++ src/operator/takeWhileInclusive.ts
@@ -0,0 +1,41 @@
+import { Operator, Observable, Subscriber } from 'rxjs';
+
+
+export function takeWhileInclusive<T>(this: Observable<T>, predicate: (value: T, index: number) => boolean): Observable<T> {
+  return this.lift(new TakeWhileInclusiveOperator(predicate));
+}
+
+class TakeWhileInclusiveOperator<T> implements Operator<T, T> {
+  constructor(private predicate: (value: T, index: number) => boolean) { }
+
+  call(subscriber: Subscriber<T>, source: any) {
+    source.subscribe(new TakeWhileInclusiveSubscriber(subscriber, this.predicate));
+  }
+}
+
+class TakeWhileInclusiveSubscriber<T> extends Subscriber<T> {
+  private index: number = 0;
+
+  constructor(destination: Subscriber<T>, private predicate: (value: T, index: number) => boolean) {
+    super(destination);
+  }
+
+  protected _next(value: T): void {
+    let result: boolean;
+
+    try {
+      result = this.predicate(value, this.index++);
+    } catch (e) {
+      this.destination.error(e);
+      return;
+    }
+
+    if (Boolean(result)) {
+      const destination = this.destination;
+      destination.next(value);
+      destination.complete();
+    } else {
+      this.destination.next(value);
+    }
+  }
+}
