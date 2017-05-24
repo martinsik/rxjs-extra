bde44cfacfbff066666678673f76587e057c4663
Martin Sikora <martin.sikora.ahoj@gmail.com>
2017-05-24 14:05:38 +0200

    more tests for reteLimit

--- src/operator/rateLimit.ts
+++ src/operator/rateLimit.ts
@@ -4,37 +4,39 @@ import { Action } from 'rxjs/scheduler/Action';
 import { Scheduler as SchedulerI } from 'rxjs/Scheduler';
 
 
-export function rateLimit<T>(this: Observable<T>, count: number, timeWindow: number, scheduler: SchedulerI = Scheduler.async): Observable<T[]> {
-  return this.lift(new RateLimitOperator(count, timeWindow, scheduler));
+export function rateLimit<T>(this: Observable<T>, count: number, timeWindow: number, emitAsap: boolean = false, scheduler: SchedulerI = Scheduler.async): Observable<T[]> {
+  return this.lift(new RateLimitOperator(count, timeWindow, emitAsap, scheduler));
 }
 
 class RateLimitOperator<T> implements Operator<T, T[]> {
-  constructor(private count: number, private timeWindow: number, private scheduler: SchedulerI) { }
+  constructor(private count: number, private timeWindow: number, private emitAsap: boolean, private scheduler: SchedulerI) { }
 
   call(subscriber: Subscriber<T[]>, source: any) {
-    return source.subscribe(new RateLimitSubscriber(subscriber, this.count, this.timeWindow, this.scheduler));
+    return source.subscribe(new RateLimitSubscriber(subscriber, this.count, this.timeWindow, this.emitAsap, this.scheduler));
   }
 }
 
 class RateLimitSubscriber<T> extends Subscriber<T> {
   private buffer: T[] = [];
-  private lastEmission: number;
   private scheduledAction: Subscription;
+  private lastEmissionTime: number = null;
 
-  constructor(destination: Subscriber<T[]>, private count: number, private timeWindow: number, private scheduler: SchedulerI) {
+  constructor(destination: Subscriber<T[]>, private count: number, private timeWindow: number, private emitAsap: boolean, private scheduler: SchedulerI) {
     super(destination);
   }
 
   private emitScheduledBuffer(state: RateLimitScheduledState<T>): void {
-    const { destination, count, timeWindow, buffer, clear, isStopped } = state;
+    const { destination, count, timeWindow, buffer, clear, isStopped, storeLastEmissionTime, now, complete } = state;
 
     let chunk = buffer.splice(0, count);
     destination.next(chunk);
 
     if (isStopped() && buffer.length === 0) {
-      destination.complete();
+      complete();
       return;
     }
+
+    storeLastEmissionTime(now());
 
     if (buffer.length === 0) {
       clear();
@@ -43,22 +45,20 @@ class RateLimitSubscriber<T> extends Subscriber<T> {
     }
   }
 
-  private clearScheduledAction() {
+  private storeLastEmissionTime(now): void {
+    this.lastEmissionTime = now;
+  }
+
+  private clearScheduledAction(): void {
     this.scheduledAction = null;
   }
 
-  private checkStopped() {
+  private checkStopped(): boolean {
     return this.isStopped;
   }
 
-  _next(value: T): void {
-    const { destination, count, timeWindow, buffer, scheduledAction, scheduler } = this;
-
-    buffer.push(value);
-
-    if (this.scheduledAction) {
-      return;
-    }
+  private scheduleBufferEmission(time: number) {
+    const {destination, count, timeWindow, buffer, scheduler} = this;
 
     const state: RateLimitScheduledState<T> = {
       timeWindow,
@@ -67,15 +67,50 @@ class RateLimitSubscriber<T> extends Subscriber<T> {
       buffer,
       clear: this.clearScheduledAction.bind(this),
       isStopped: this.checkStopped.bind(this),
+      storeLastEmissionTime: this.storeLastEmissionTime.bind(this),
+      now: scheduler.now.bind(this),
+      complete: this.doComplete.bind(this)
     };
 
-    this.scheduledAction = scheduler.schedule(this.emitScheduledBuffer, timeWindow, state);
+    this.scheduledAction = scheduler.schedule(this.emitScheduledBuffer, time, state);
   }
 
-  _complete() {
-    const { buffer } = this;
-    if (buffer.length === 0) {
-      super._complete();
+  protected  _next(value: T): void {
+    const { lastEmissionTime, buffer, timeWindow } = this;
+    const now = this.scheduler.now();
+
+    if (this.emitAsap && buffer.length === 0) {
+      if ((now - lastEmissionTime) >= timeWindow || lastEmissionTime === null) {
+        this.destination.next([value]);
+        this.lastEmissionTime = now;
+
+        return;
+      }
+    }
+
+    buffer.push(value);
+
+    if (!this.scheduledAction) {
+      let delay;
+
+      if (this.emitAsap) {
+        const timeSinceLastEmission = now - lastEmissionTime;
+        delay = lastEmissionTime === null ? timeWindow : Math.min(timeWindow, lastEmissionTime + timeWindow - now);
+      } else {
+        delay = timeWindow;
+      }
+
+      this.scheduleBufferEmission(delay);
+    }
+  }
+
+  private doComplete() {
+    super._complete();
+  }
+
+  protected _complete() {
+    if (this.buffer.length === 0) {
+      this.doComplete();
     }
   }
 
@@ -88,4 +123,7 @@ type RateLimitScheduledState<T> = {
   buffer: T[];
   clear: Function;
   isStopped: Function;
+  storeLastEmissionTime: Function;
+  now: Function;
+  complete: Function;
 }
--- test/operator/rateLimit-test.ts
+++ test/operator/rateLimit-test.ts
@@ -1,9 +1,9 @@
 import * as Rx from 'rxjs';
 import '../../dist/cjs/index';
 import {expect} from 'chai';
-import marbleTestingSignature = require('../helpers/marble-testing'); // tslint:disable-line:no-require-imports
+import marbleTestingSignature = require('../helpers/marble-testing');
 
-declare const { asDiagram };
+declare const { asDiagram, time };
 declare const hot: typeof marbleTestingSignature.hot;
 declare const cold: typeof marbleTestingSignature.cold;
 declare const expectObservable: typeof marbleTestingSignature.expectObservable;
@@ -12,29 +12,30 @@ declare const rxTestScheduler: Rx.TestScheduler;
 const Observable = Rx.Observable;
 
 describe('Observable.prototype.rateLimit', () => {
-
   asDiagram('rateLimit(3, 100)')('should re-emit each chunk with 100 delay', () => {
-    //                  ----------
-    //                            xx----------
-    //                                        ----------
-    const source = hot('1--2-3------4-5678-----9--|');
-    const expected =   '----------x-----------y---------(z|)';
+    //                  x----------            ----------
+    //                             xx----------
+    const t = time('----------|');
+    const source = hot('-1--2-3------4-5678------9--|');
+    const expected =   '-----------x-----------y---------(z|)';
+    const subs =       '^                                !';
     const values = {
       x: ['1', '2', '3'],
       y: ['4', '5', '6'],
       z: ['7', '8', '9'],
     };
 
-    expectObservable(source.rateLimit(3, 100, rxTestScheduler)).toBe(expected, values);
+    expectObservable(source.rateLimit(3, t, false, rxTestScheduler)).toBe(expected, values);
+    expectSubscriptions(source.subscriptions).toBe(subs);
   });
 
   it('should split very fast emitting source into chunks with constant delay', () => {
+    //                  ----------          ----------
+    //                            ----------          ----------
     const source = hot('abcdefghijklmnopqr|');
-    //                  ----------
-    //                            ----------
-    //                                      ----------
-    //                                                ----------
     const expected =   '----------x---------y---------z---------(u|)';
+    const subs =       '^                                       !';
+    const t =     time('----------|');
     const values = {
       x: ['a', 'b', 'c', 'd', 'e'],
       y: ['f', 'g', 'h', 'i', 'j'],
@@ -42,7 +43,140 @@ describe('Observable.prototype.rateLimit', () => {
       u: ['p', 'q', 'r'] ,
     };
 
-    expectObservable(source.rateLimit(5, 100, rxTestScheduler)).toBe(expected, values);
+    expectObservable(source.rateLimit(5, t, false, rxTestScheduler)).toBe(expected, values);
+    expectSubscriptions(source.subscriptions).toBe(subs);
   });
 
+  it('should complete when the source is empty', () => {
+    const source = hot('--|');
+    const expected =   '--|';
+    const subs =       '^ !';
+    const t =     time('----------|');
+
+    expectObservable(source.rateLimit(5, t, false, rxTestScheduler)).toBe(expected);
+    expectSubscriptions(source.subscriptions).toBe(subs);
+  });
+
+  it('should only once if the source emission fir into a single window', () => {
+    //                  ----------
+    //                            ----------
+    const source = hot('a-bc--------------------|');
+    const expected =   '----------x-------------|';
+    const subs =       '^                       !';
+    const t =     time('----------|');
+    const values = {
+      x: ['a', 'b', 'c'],
+    };
+
+    expectObservable(source.rateLimit(5, t, false, rxTestScheduler)).toBe(expected, values);
+    expectSubscriptions(source.subscriptions).toBe(subs);
+  });
+
+  it('should propagate error immediately', () => {
+    const err = new Error();
+    const source = hot('a-bc--#', undefined, err);
+    const expected =   '------#';
+    const subs =       '^     !';
+    const t =     time('----------|');
+
+    expectObservable(source.rateLimit(5, t, false, rxTestScheduler)).toBe(expected, undefined, err);
+    expectSubscriptions(source.subscriptions).toBe(subs);
+  });
+
+  it('should emit the first value immediately even after long delay', () => {
+    //                  -----     x-----     xx-----
+    //                       -----      -----
+    const source = hot('12-3-------4-567-------8------|');
+    const expected =   '-----x----------y-----------z-|';
+    const subs =       '^                             !';
+    const t =     time('-----|');
+    const values = {
+      x: ['1', '2', '3'],
+      y: ['4', '5', '6', '7'],
+      z: ['8'],
+    };
+
+    expectObservable(source.rateLimit(5, t, false, rxTestScheduler)).toBe(expected, values);
+    expectSubscriptions(source.subscriptions).toBe(subs);
+  });
+
+  it('should not emit before subscription', () => {
+    //                                  xx----------
+    //                        ----------            ----------
+    const source = hot('1--^--2-3---------4----|');
+    const expected =      '-------------x-----------(y|)';
+    const subs =          '^                        !';
+    const t =     time('----------|');
+    const values = {
+      x: ['2', '3'],
+      y: ['4'],
+    };
+
+    expectObservable(source.rateLimit(5, t, false, rxTestScheduler)).toBe(expected, values);
+    expectSubscriptions(source.subscriptions).toBe(subs);
+  });
+
+  it('should complete immediately if there are no more buffered items', () => {
+    //                        -----     xx-----     ---|
+    //                   -----     -----       -----
+    const source = cold('1----------------2------------|');
+    const expected =    '-----x----------------y-------|';
+    const subs =        '^                             !';
+    const t =     time('-----|');
+    const values = {
+      x: ['1'],
+      y: ['2'],
+    };
+
+    expectObservable(source.rateLimit(5, t, false, rxTestScheduler)).toBe(expected, values);
+    expectSubscriptions(source.subscriptions).toBe(subs);
+  });
+
+
+  it('should emit the first value immediately after receiving it when asap mode enabled', () => {
+    //                  x          ----------
+    //                   ----------          ----------
+    const source = hot('-1-2-3------4-5678-----9--|');
+    const expected =   '-x---------y---------z---------(i|)';
+    const subs =       '^                              !';
+    const t =     time('----------|');
+    const values = {
+      x: ['1'],
+      y: ['2', '3'],
+      z: ['4', '5', '6', '7', '8'],
+      i: ['9'],
+    };
+
+    expectObservable(source.rateLimit(5, t, true, rxTestScheduler)).toBe(expected, values);
+    expectSubscriptions(source.subscriptions).toBe(subs);
+  });
+
+  it('should emit the first value immediately even after long delay when asap mode enabled', () => {
+    //                  x-----     x     -----
+    //                        ----- xx---     -----
+    const source = hot('-12-3-------4-567-8--9----|');
+    const expected =   '-x----y-----z----i----j---|';
+    const subs =       '^                         !';
+    const t =     time('-----|');
+    const values = {
+      x: ['1'],
+      y: ['2', '3'],
+      z: ['4'],
+      i: ['5', '6', '7'],
+      j: ['8', '9'],
+    };
+
+    expectObservable(source.rateLimit(5, t, true, rxTestScheduler)).toBe(expected, values);
+    expectSubscriptions(source.subscriptions).toBe(subs);
+  });
+
+  it('should complete when the source is empty when asap mode enabled', () => {
+    const source = hot('--|');
+    const expected =   '--|';
+    const subs =       '^ !';
+    const t =     time('----------|');
+
+    expectObservable(source.rateLimit(5, t, true, rxTestScheduler)).toBe(expected);
+    expectSubscriptions(source.subscriptions).toBe(subs);
+  });
 });
