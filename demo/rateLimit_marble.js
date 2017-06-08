446616a287c87a06bba725eb90820e20415c4f45
Martin Sikora <martin.sikora.ahoj@gmail.com>
2017-06-08 22:16:09 +0200

    more rateLimit demo

--- demo/rateLimit_marble.js
+++ demo/rateLimit_marble.js
@@ -1,7 +1,12 @@
 const TestScheduler = require('rxjs/testing/TestScheduler').TestScheduler;
 const Observable = require('rxjs/Observable').Observable;
 const chai = require('chai');
+require('rxjs/add/observable/of');
 require('rxjs/add/operator/do');
+require('rxjs/add/operator/delay');
+require('rxjs/add/operator/filter');
+require('rxjs/add/operator/concatMap');
+require('rxjs/add/operator/bufferTime');
 require('../dist/cjs/add/operator/rateLimit');
 
 const delayMarbles =                 '----------|';
@@ -11,18 +16,27 @@ const inputMarbles =                 '--1-2-3-------------4--5---6------------7-
 const expectedRateLimitMarbles =     '------------x-----------------y-------------------(z|)';
 //                                    xx----------        xx----------          ----------
 //                                      xxxxxxxxxx--------            ----------
-const expectedRateLimitMarblesAsap = '--x---------y---------z---------i---------(j|)';
+const expectedRateLimitAsapMarbles = '--x---------y---------z---------i---------(j|)';
+//                                      xxxx----------xxxxxxxxxx----------xxxxxxxxxx----------
+const expectedBufferTimeMarbles =    '----------------x-------------------y---------z---------(i|)';
+
 const values1 = {
     x: ['1', '2', '3'],
     y: ['4', '5', '6'],
     z: ['7'],
 };
 const values2 = {
-    x: ['1', ],
+    x: ['1'],
     y: ['2', '3'],
     z: ['4'],
     i: ['5', '6'],
     j: ['7'],
+};
+const values3 = {
+    x: ['1', '2', '3'],
+    y: ['4', '5'],
+    z: ['6'],
+    i: ['7'],
 };
 const scheduler = new TestScheduler(chai.assert.deepEqual);
 const t = scheduler.createTime(delayMarbles);
@@ -31,6 +45,12 @@ source1 = scheduler.createHotObservable(inputMarbles).rateLimit(3, t, false, sch
 scheduler.expectObservable(source1.do(null, null, () => console.log('complete1'))).toBe(expectedRateLimitMarbles, values1);
 
 source2 = scheduler.createHotObservable(inputMarbles).rateLimit(3, t, true, scheduler);
-scheduler.expectObservable(source2.do(null, null, () => console.log('complete2'))).toBe(expectedRateLimitMarblesAsap, values2);
+scheduler.expectObservable(source2.do(null, null, () => console.log('complete2'))).toBe(expectedRateLimitAsapMarbles, values2);
+
+source3 = scheduler.createHotObservable(inputMarbles)
+    .bufferTime(t, null, 3, scheduler)
+    .filter(array => array.length !== 0)
+    .concatMap(buffer => Observable.of(buffer).delay(t, scheduler));
+scheduler.expectObservable(source3.do(null, null, () => console.log('complete3'))).toBe(expectedBufferTimeMarbles, values3);
 
 scheduler.flush();
