1818f65ec2fbfc584aaee6bab6b81853d8a6d32e
Martin Sikora <martin.sikora.ahoj@gmail.com>
2019-03-03 18:27:38 +0100

    more tests for finalizeWithReason

--- spec/marble-testing.ts
+++ spec/marble-testing.ts
@@ -1,8 +1,8 @@
-import { assert } from "chai";
+import { assert } from 'chai';
 
 declare const global: any;
 
 export const cold = global.cold;
-export const hot = global.createHotObservable;
+export const hot = global.hot;
 export const expectObservable = global.expectObservable;
 export const expectSubscriptions = global.expectSubscriptions;
