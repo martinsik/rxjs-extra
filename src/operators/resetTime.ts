51d9cb3171e1d87114ddb45e8fe47058fd1232e2
Martin Sikora <martin.x.sikora@pwc.com>
2019-12-31 19:09:19 +0100

    add resetTime comment

--- src/operators/resetTime.ts
+++ src/operators/resetTime.ts
@@ -5,6 +5,14 @@ type ResetWithFunc = <T, N>(value: T | N) => N;
 
 export function resetTime<T>(resetDelay: number, resetWith: T | ((v: T) => T), scheduler?: SchedulerLike): MonoTypeOperatorFunction<T>;
 
+/**
+ * Operator that mirrors the source Observable but after every `next` notification emits another `resetWith`
+ * next notification with set delay.
+ *
+ * @param resetDelay number
+ * @param resetWith any | (value: any) => any;
+ * @param scheduler
+ */
 export function resetTime<T, N>(
   resetDelay: number,
   resetWith: N | ResetWithFunc,
